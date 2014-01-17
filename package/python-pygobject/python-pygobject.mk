7b7d185f03aa513ab2812c81c353ce77094d1d4a
Eric Le Bihan <eric.le.bihan.dev@free.fr>
2014-01-17 16:18:21 +0100

    python-pygobject: new package.
    python-pygobject provides Python bindings for GLib, GObject and GIO.
    
    This is the old version of the bindings, used by PyGtk and *not* the
    new version which relies on gobject-introspection.

---
+++ package/python-pygobject/python-pygobject.mk
@@ -0,0 +1,31 @@
+################################################################################
+#
+# python-pygobject
+#
+################################################################################
+
+PYTHON_PYGOBJECT_VERSION_MAJOR = 2.28
+PYTHON_PYGOBJECT_VERSION_MINOR = 6
+PYTHON_PYGOBJECT_VERSION = $(PYTHON_PYGOBJECT_VERSION_MAJOR).$(PYTHON_PYGOBJECT_VERSION_MINOR)
+PYTHON_PYGOBJECT_SITE = http://ftp.gnome.org/pub/GNOME/sources/pygobject/$(PYTHON_PYGOBJECT_VERSION_MAJOR)/
+PYTHON_PYGOBJECT_SOURCE = pygobject-$(PYTHON_PYGOBJECT_VERSION).tar.bz2
+PYTHON_PYGOBJECT_LICENSE = LGPLv2.1+
+PYTHON_PYGOBJECT_LICENSE_FILES = COPYING
+
+PYTHON_PYGOBJECT_DEPENDENCIES = host-pkgconf host-python python libglib2
+
+PYTHON_PYGOBJECT_INSTALL_STAGING = YES
+
+PYTHON_PYGOBJECT_CONF_OPT = --enable-introspection=no \
+			  --enable-cairo=no \
+			  --disable-ffi
+
+# Fixup path for codegen and defs
+define PYTHON_PYGOBJECT_FIXUP_PC_FILE
+	$(SED) 's|=$${datadir}/|=$(STAGING_DIR)/usr/share/|' \
+		$(STAGING_DIR)/usr/lib/pkgconfig/pygobject-2.0.pc
+endef
+
+PYTHON_PYGOBJECT_POST_INSTALL_STAGING_HOOKS += PYTHON_PYGOBJECT_FIXUP_PC_FILE
+
+$(eval $(autotools-package))
