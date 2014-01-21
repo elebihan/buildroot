e02547585e74e6de3892acc533b959452ccc4216
Eric Le Bihan <eric.le.bihan.dev@free.fr>
2014-01-21 14:28:03 +0100

    python-pycairo: new package.
    python-pycairo offers Python bindings for cairo.
    
    The package is built using the autotools and not waf due to
    cross-compiling issues.

---
+++ package/python-pycairo/python-pycairo.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# python-pycairo
+#
+################################################################################
+
+PYTHON_PYCAIRO_VERSION = 1.10.0
+PYTHON_PYCAIRO_SITE = http://cairographics.org/releases/
+PYTHON_PYCAIRO_SOURCE = py2cairo-$(PYTHON_PYCAIRO_VERSION).tar.bz2
+PYTHON_PYCAIRO_LICENSE = Dual LGPLv2.1+/MPLv1.1
+PYTHON_PYCAIRO_LICENSE_FILES = COPYING-LGPL-2.1 COPYING-MPL-1.1
+
+PYTHON_PYCAIRO_DEPENDENCIES = host-pkgconf cairo python-xpyb
+
+PYTHON_PYCAIRO_INSTALL_STAGING = YES
+
+PYTHON_PYCAIRO_AUTORECONF=YES
+
+$(eval $(autotools-package))
