26a3ec8ad4bf28e9f15296d5db9f22c9eaf7154d
Eric Le Bihan <eric.le.bihan.dev@free.fr>
2014-01-21 10:49:59 +0100

    python-xpyb: new package.
    python-xpyb offers Python bindings for XCB, the X protocol C-language
    binding.

---
+++ package/python-xpyb/python-xpyb.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# python-xpyb
+#
+################################################################################
+
+PYTHON_XPYB_VERSION = 1.3.1
+PYTHON_XPYB_SITE = http://xcb.freedesktop.org/dist/
+PYTHON_XPYB_SOURCE = xpyb-$(PYTHON_XPYB_VERSION).tar.bz2
+PYTHON_XPYB_LICENSE = Public domain
+PYTHON_XPYB_LICENSE_FILES = COPYING
+PYTHON_XPYB_DEPENDENCIES = host-pkgconf host-python python xproto_xproto xcb-proto
+PYTHON_XPYB_INSTALL_STAGING=YES
+
+$(eval $(autotools-package))
