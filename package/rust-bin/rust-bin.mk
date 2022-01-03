1b35b58a9395dd701d8c21f2f34ea561bd752074
Eric Le Bihan <eric.le.bihan.dev@free.fr>
2022-01-03 22:12:03 +0100

    rust-bin: fix

--- package/rust-bin/rust-bin.mk
+++ package/rust-bin/rust-bin.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RUST_BIN_VERSION = nightly-2021-09-20
+RUST_BIN_VERSION = nightly
 RUST_BIN_SITE = https://static.rust-lang.org/dist
 RUST_BIN_LICENSE = Apache-2.0 or MIT
 RUST_BIN_LICENSE_FILES = LICENSE-APACHE LICENSE-MIT
